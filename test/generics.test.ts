e45a6924e09a85f06f9f876b57902ec480525389
Tanner Nielsen <tannerntannern@gmail.com>
2018-10-24 15:46:29 -0500

    Added a generics test case.

---
+++ test/generics.test.ts
@@ -0,0 +1,19 @@
+import 'mocha';
+import {expect} from 'chai';
+import Mixin from '../src/mixins';
+
+// class GenericClassA<T> {
+// 	testA(input: T) {
+//
+// 	}
+// }
+//
+// class GenericClassB<T> {
+// 	testB(input: T) {
+//
+// 	}
+// }
+//
+// class MyMixed<TA, TB> extends Mixin<GenericClassA<TA>, GenericClassB<TB>>(GenericClassA, GenericClassB) {
+//
+// }
