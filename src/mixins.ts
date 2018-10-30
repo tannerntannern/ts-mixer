35d078156f8c0cf4b7935d1c53d18d511a2aa964
Tanner Nielsen <tannerntannern@gmail.com>
2018-10-30 11:26:08 -0500

    Added quick support for typedoc.

--- src/mixins.ts
+++ src/mixins.ts
@@ -1,5 +1,3 @@
-// Based on https://github.com/Microsoft/TypeScript/pull/13743#issuecomment-429426722
-
 /**
  * Utility function that returns the full chain of prototypes (excluding Object.prototype) from the given prototype.
  * The order will be [proto, protoParent, protoGrandparent, ...]
@@ -15,10 +13,20 @@ function getProtoChain(proto: object): object[] {
 	return protoChain;
 }
 
-// Constructor type
+/**
+ * Basic constructor type
+ */
 type Constructor<T> = new(...args: any[]) => T;
 
-// Overloads for up to 10 classes; it is exceedingly unlikely that you'll ever need to mix more than 10 classes at once
+/**
+ * The main mixin function; takes a number of constructors, and returns a new class that
+ * 	1) applies each of the given constructors in its own constructor
+ * 	2) applies each prototype (and parent prototypes) of the given constructors to its own prototype
+ * 	3) links any static properties on the given constructors to the class via getters and setters
+ *
+ * Note that there are overloads for up to 10 classes.  If you go beyond 10 classes the "JavaScript side" will still
+ * work, but the "TypeScript side" will not.
+ */
 function Mixin<C1>(ctor1: Constructor<C1>): Constructor<C1>;
 function Mixin<C1, C2>(ctor1: Constructor<C1>, ctor2: Constructor<C2>): Constructor<C1 & C2>;
 function Mixin<C1, C2, C3>(ctor1: Constructor<C1>, ctor2: Constructor<C2>, ctor3: Constructor<C3>): Constructor<C1 & C2 & C3>;
@@ -29,8 +37,6 @@ function Mixin<C1, C2, C3, C4, C5, C6, C7>(ctor1: Constructor<C1>, ctor2: Constr
 function Mixin<C1, C2, C3, C4, C5, C6, C7, C8>(ctor1: Constructor<C1>, ctor2: Constructor<C2>, ctor3: Constructor<C3>, ctor4: Constructor<C4>, ctor5: Constructor<C5>, ctor6: Constructor<C6>, ctor7: Constructor<C7>, ctor8: Constructor<C8>): Constructor<C1 & C2 & C3 & C4 & C5 & C6 & C7 & C8>;
 function Mixin<C1, C2, C3, C4, C5, C6, C7, C8, C9>(ctor1: Constructor<C1>, ctor2: Constructor<C2>, ctor3: Constructor<C3>, ctor4: Constructor<C4>, ctor5: Constructor<C5>, ctor6: Constructor<C6>, ctor7: Constructor<C7>, ctor8: Constructor<C8>, ctor9: Constructor<C9>): Constructor<C1 & C2 & C3 & C4 & C5 & C6 & C7 & C8 & C9>;
 function Mixin<C1, C2, C3, C4, C5, C6, C7, C8, C9, C10>(ctor1: Constructor<C1>, ctor2: Constructor<C2>, ctor3: Constructor<C3>, ctor4: Constructor<C4>, ctor5: Constructor<C5>, ctor6: Constructor<C6>, ctor7: Constructor<C7>, ctor8: Constructor<C8>, ctor9: Constructor<C9>, ctor10: Constructor<C10>): Constructor<C1 & C2 & C3 & C4 & C5 & C6 & C7 & C8 & C9 & C10>;
-
-// Actual mixin implementation
 function Mixin(...constructors: Constructor<any>[]) {
 	class MixedClass {
 		// Apply each of the mixing class constructors
@@ -74,7 +80,22 @@ function Mixin(...constructors: Constructor<any>[]) {
 	return MixedClass;
 }
 
-// Mixin decorator for special generic use cases
+/**
+ * A decorator version of the `Mixin` function.  Instead of applying mixins like this,
+ *
+ * 	class MyClass extends Mixin(Mixin1, Mixin2, Mixin3) {}
+ *
+ * you can apply mixins like this instead:
+ *
+ * 	@MixinDecorator(Mixin1, Mixin2, Mixin3)
+ * 	class MyClass {}
+ *
+ * 	// This line is necessary for the typings to work
+ * 	interface MyClass extends Mixin1, Mixin2, Mixin3 {}
+ *
+ * The main reason you might want to use this over `Mixin` is for mixing generic classes.  See the main README for more
+ * information.
+ */
 function MixinDecorator(...constructors: Constructor<any>[]) {
 	return function<T>(baseClass: T): T {
 		// @ts-ignore
