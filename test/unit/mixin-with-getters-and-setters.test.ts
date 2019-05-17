c9e61c06e9bdd95ba98f3c1e640ac022f84066c9
Tanner Nielsen <tannerntannern@gmail.com>
2019-05-16 22:51:56 -0500

    Added a basic test for the new getters and setters feature.

--- test/unit/mixin-with-getters-and-setters.test.ts
+++ test/unit/mixin-with-getters-and-setters.test.ts
@@ -3,5 +3,27 @@ import {expect} from 'chai';
 import {Mixin} from '../../src/mixins';
 
 describe('Mixins with getters and setters', () => {
-	// TODO: ...
+	it('should copy props correctly when the prototype has a getter/setter', () => {
+		let externalValue = 0;
+
+		class Base {}
+		class HasGetter {
+			public get externalValue() {
+				return externalValue;
+			}
+		}
+
+		const Mixed = Mixin(Base, HasGetter);
+
+		let hg = new HasGetter();
+		let mx = new Mixed();
+
+		expect(hg.externalValue).to.equal(0);
+		expect(mx.externalValue).to.equal(0);
+
+		externalValue ++;
+
+		expect(hg.externalValue).to.equal(1);
+		expect(mx.externalValue).to.equal(1);
+	});
 });
