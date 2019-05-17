836f269b4b880a7449a6ae55648eef2b4e12cfcc
Tanner Nielsen <tannerntannern@gmail.com>
2019-05-16 22:18:11 -0500

    Tested the static properties cases a little better.

--- test/unit/static-props.test.ts
+++ test/unit/static-props.test.ts
@@ -20,6 +20,10 @@ class JumperMixin {}
 class LongJumper extends Mixin(Person, RunnerMixin, JumperMixin) {}
 
 describe('Static properties', function(){
+	beforeEach(() => {
+		Person.TOTAL = 0;
+	});
+
 	it('should update base class static prop when the mixed class changes it', function(){
 		// Invoke the TOTAL incrementer from two different constructors
 		new Person('Bob');
@@ -27,5 +31,19 @@ describe('Static properties', function(){
 
 		expect(Person.TOTAL).to.equal(2);
 		expect(LongJumper.TOTAL).to.equal(2);
+	});
+	
+	it('setting the static prop on the mixed class should change the base class prop as well', () => {
+		LongJumper.TOTAL = 20;
+
+		expect(Person.TOTAL).to.equal(20);
+		expect(LongJumper.TOTAL).to.equal(20);
+	});
+
+	it('setting the static prop on the base class should change the mixed class prop as well', () => {
+		Person.TOTAL = 45;
+
+		expect(Person.TOTAL).to.equal(45);
+		expect(LongJumper.TOTAL).to.equal(45);
 	});
 });
