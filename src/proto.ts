08ea25327c3162dbdaee3e68009c07f373507025
Tanner Nielsen <tannerntannern@gmail.com>
2019-12-27 08:40:40 -0600

    chore: adds and tests uniqueAncestors function

--- src/proto.ts
+++ src/proto.ts
@@ -19,7 +19,7 @@ export const ancestors = (obj: object) => protoChain(obj).map(proto => proto.con
  * Identifies the nearest ancestor common to all the given objects in their prototype chains.  For most unrelated
  * objects, this function should return Object.
  */
-export const nearestCommonAncestor = (...objs: object[]): object => {
+export const nearestCommonAncestor = (...objs: object[]): Function => {
 	if (objs.length === 0) return undefined;
 
 	let commonAncestor = undefined;
@@ -36,4 +36,33 @@ export const nearestCommonAncestor = (...objs: object[]): object => {
 	}
 
 	return commonAncestor;
+};
+
+/**
+ * Computes the ancestor chain for each of the given objects and filters out any ancestors that are common to each.  The
+ * results is a list of ancestor chains in the same order as the given objects.
+ */
+export const uniqueAncestors = (...objs: object[]): Function[][] => {
+	if (objs.length === 0) return [];
+
+	const ancestorChains = objs.map(obj => ancestors(obj));
+	if (objs.length === 1) return ancestorChains;
+
+	// the number of filtered ancestors can't be larger than the total ancestors in any one of the chains, so we just
+	// use the first one to check against the others
+	const firstAncestorChain = ancestorChains[0].slice();
+	for (let potentialSharedAncestor of firstAncestorChain) {
+		const indicesOfSharedAncestor = ancestorChains.map(ancestorChain => ancestorChain.indexOf(potentialSharedAncestor));
+		const everyChainContainsPotentialSharedAncestor = indicesOfSharedAncestor.every(index => index != -1);
+
+		if (everyChainContainsPotentialSharedAncestor) {
+			for (let i = 0; i < ancestorChains.length; i ++) {
+				const ancestorChain = ancestorChains[i];
+				const indexInAncestorChain = indicesOfSharedAncestor[i];
+				ancestorChain.splice(indexInAncestorChain, 1);
+			}
+		}
+	}
+
+	return ancestorChains;
 };
